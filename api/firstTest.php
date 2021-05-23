5d688c2b49fec5c6aff1dd96a6bc98ed08fe6faa
kdcrist <56696111+kdcrist@users.noreply.github.com>
2021-05-23 10:15:29 -0400

    Create firstTest.php

---
+++ api/firstTest.php
@@ -0,0 +1,37 @@
+<?php
+
+	require_once 'dbh-inc.php';
+
+	$data = json_decode(file_get_contents('php://input'),true);
+    $returnData = "";
+    $numItems = 0;
+
+    $sql = "SELECT FirstName FROM Users;";
+	$stmt = mysqli_stmt_init($conn);
+	if (!mysqli_stmt_prepare($stmt, $sql)) {
+		echo"sql stmt did not prepare";
+		exit();
+	}
+
+	mysqli_stmt_bind_param($stmt);
+	mysqli_stmt_execute($stmt);
+
+	$result_data = mysqli_stmt_get_result($stmt);
+
+    
+    while ($row = mysqli_fetch_assoc($result_data)) {
+        if($numItems > 0){
+            $returnData .= ", ";
+        }
+        $returnData .= '{"FirstName":'. $row["FirstName"] .'"}';
+        $numItems++;
+        
+	}
+
+    if($numItems < 1){
+        returnWithError("No Users");
+        exit();
+    }else{
+        $return  = '{"result":[ '. $returnData . ' ]}';
+        sendResultInfoAsJson( $return ); 
+    }
