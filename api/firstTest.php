aa44d5de4ff55ae9e043a8ee6c9ac682ef6a0079
kdcrist <56696111+kdcrist@users.noreply.github.com>
2021-05-23 10:34:50 -0400

    Update firstTest.php

--- api/firstTest.php
+++ api/firstTest.php
@@ -5,15 +5,17 @@
 	$data = json_decode(file_get_contents('php://input'),true);
     $returnData = "";
     $numItems = 0;
+	
+	$FirstName = $data["FirstName"];
 
-    $sql = "SELECT FirstName FROM Users;";
+    $sql = "SELECT LastName FROM Users WHERE FirstName = ?;";
 	$stmt = mysqli_stmt_init($conn);
 	if (!mysqli_stmt_prepare($stmt, $sql)) {
 		echo"sql stmt did not prepare";
 		exit();
 	}
 
-	mysqli_stmt_bind_param($stmt);
+	mysqli_stmt_bind_param($stmt, "i", $FirstName);
 	mysqli_stmt_execute($stmt);
 
 	$result_data = mysqli_stmt_get_result($stmt);
@@ -23,7 +25,7 @@
         if($numItems > 0){
             $returnData .= ", ";
         }
-        $returnData .= '{"FirstName":'. $row["FirstName"] .'"}';
+        $returnData .= '{"LastName":'. $row["LastName"] .'"}';
         $numItems++;
         
 	}
