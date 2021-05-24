cbfea813460cc34352116b9e877096951450faba
Anthonyjackson <56099042+Anthonyjackson@users.noreply.github.com>
2021-05-24 18:33:57 -0400

    Update AddContact.php
    Trying out new add

--- api/AddContact.php
+++ api/AddContact.php
@@ -1,3 +1,4 @@
+  
 <?php
 require 'DBH.php';
 require 'functions.php';
@@ -21,13 +22,22 @@ if ($conn->connect_error)
 }
 else
 {
+  # payload should look like this: { "ID": 0, "FirstName": "First", "LastName": "Last", "StreetAddress": "address", "City": "someCity }
+  
+ 
+  
+  
+  
+  $sql = "insert into Contacts (ID, FirstName, LastName, StreetAddress, City,State, ZipCode, PhoneNumber, Email) VALUES ('" . $ID . "', '" . $FirstName . "', '" . $LastName . "', '" . $StreetAddress . "', '" . $City . 
+    "', '" . $State . "', '" . $ZipCode . "', '" . $PhoneNumber . "',  '" . $Email . ") ";
 	# insert into Contacts (FirstName,LastName,StreetAddress,City,State,ZipCode,PhoneNumber,Email) VALUES ('Jessica', 'Jones','112 house avenue','',3);
-	$stmt = $conn->prepare("INSERT into Contacts (ID,FirstName,LastName,StreetAddress,City,State,ZipCode,PhoneNumber,Email) VALUES(?,?,?,?,?,?,?,?,?)");
-	$stmt->bind_param("issssssss", $ID, $FirstName, $LastName, $StreetAddress, $City, $State, $ZipCode, $PhoneNumber, $Email);
-	$stmt->execute();
-	$stmt->close();
+  #what was taught in class
+	#$stmt = $conn->prepare("INSERT into Contacts (ID,FirstName,LastName,StreetAddress,City,State,ZipCode,PhoneNumber,Email) VALUES(?,?,?,?,?,?,?,?,?)");
+	#$stmt->bind_param("issssssss", $ID, $FirstName, $LastName, $StreetAddress, $City, $State, $ZipCode, $PhoneNumber, $Email);
+	#$stmt->execute();
+	#$stmt->close();
 	$conn->close();
-	returnWithError("");
+	returnWithError("we did it!");
 }
 
 ?>
