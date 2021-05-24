2d0ed63a03f4df98d354528ec88ff6c040cf1da5
nikhilmsreedhar <65008600+nikhilmsreedhar@users.noreply.github.com>
2021-05-24 15:54:54 -0400

    Create RemoveContact.php

---
+++ api/RemoveContact.php
@@ -0,0 +1,47 @@
+<?php
+
+$inData = getRequestInfo();
+
+$serverName = "192.3.62.202";
+$dBUsername = "API";
+$dBPassword = "I4m4robot!";
+$dBName = "cop4331_database";
+
+$ID = $inData["ID"];
+$FirstName = $inData["FirstName"];
+$LastName = $inData["LastName"];
+
+$conn = new mysqli($serverName, $dBUsername, $dBPassword, $dBName);
+if ($conn->connect_error)
+{
+    returnWithError( $conn->connect_error );
+}
+else
+{
+    # insert into Contacts (FirstName,LastName,StreetAddress,City,State,ZipCode,PhoneNumber,Email) VALUES ('Jessica', 'Jones','112 house avenue','',3);
+    $stmt = $conn->prepare("DELETE FROM Contacts WHERE ID =? AND FirstName =? AND LastName =?");
+    $stmt->bind_param("sss", $ID, $FirstName, $LastName);
+    $stmt->execute();
+    $stmt->close();
+    $conn->close();
+    returnWithError("");
+}
+
+function getRequestInfo()
+{
+    return json_decode(file_get_contents('php://input'), true);
+}
+
+function sendResultInfoAsJson( $obj )
+{
+    header('Content-type: application/json');
+    echo $obj;
+}
+
+function returnWithError( $err )
+{
+    $retValue = '{"error":"' . $err . '"}';
+    sendResultInfoAsJson( $retValue );
+}
+
+?>
