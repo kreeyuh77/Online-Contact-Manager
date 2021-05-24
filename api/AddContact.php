1934a4f8d034239645147e4096cb30b81f4107e4
nikhilmsreedhar <65008600+nikhilmsreedhar@users.noreply.github.com>
2021-05-24 14:14:02 -0400

    Update AddContact.php

--- api/AddContact.php
+++ api/AddContact.php
@@ -1,51 +1,53 @@
+
 <?php
-	$inData = getRequestInfo();
+$inData = getRequestInfo();
 
-	$serverName = "192.3.62.202";
-	$dBUsername = "API";
-	$dBPassword = "I4m4robot!";
-	$dBName = "cop4331_database";
+$serverName = "192.3.62.202";
+$dBUsername = "API";
+$dBPassword = "I4m4robot!";
+$dBName = "cop4331_database";
 
-	$FirstName = $inData["FirstName"];
-	$LastName = $inData["LastName"];
-	$StreetAddress = $inData["StreetAddress"];
-	$City = $inData["City"];
-	$State = $inData["State"];
-	$ZipCode = $inData["ZipCode"];
-	$PhoneNumber= $inData["PhoneNumber"];
-	$Email = $inData["Email"];
+$ID = $inData["ID"];
+$FirstName = $inData["FirstName"];
+$LastName = $inData["LastName"];
+$StreetAddress = $inData["StreetAddress"];
+$City = $inData["City"];
+$State = $inData["State"];
+$ZipCode = $inData["ZipCode"];
+$PhoneNumber= $inData["PhoneNumber"];
+$Email = $inData["Email"];
 
-	$conn = new mysqli('localhost', $dBUsername, $dBPassword, $dBName);
-	if ($conn->connect_error)
-	{
-		returnWithError( $conn->connect_error );
-	}
-	else
-	{
-		# insert into Contacts (FirstName,LastName,StreetAddress,City,State,ZipCode,PhoneNumber,Email) VALUES ('Jessica', 'Jones','112 house avenue','',3);
-		$stmt = $conn->prepare("INSERT into Contacts (FirstName,LastName,StreetAddress,City,State,ZipCode,PhoneNumber,Email) VALUES(?,?,?,?,?,?,?,?)");
-		$stmt->bind_param("ssssssss", $FirstName, $LastName, $StreetAddress, $City, $State, $ZipCode, $PhoneNumber, $Email);
-		$stmt->execute();
-		$stmt->close();
-		$conn->close();
-		returnWithError("");
-	}
+$conn = new mysqli($serverName, $dBUsername, $dBPassword, $dBName);
+if ($conn->connect_error)
+{
+	returnWithError( $conn->connect_error );
+}
+else
+{
+	# insert into Contacts (FirstName,LastName,StreetAddress,City,State,ZipCode,PhoneNumber,Email) VALUES ('Jessica', 'Jones','112 house avenue','',3);
+	$stmt = $conn->prepare("INSERT into Contacts (ID,FirstName,LastName,StreetAddress,City,State,ZipCode,PhoneNumber,Email) VALUES(?,?,?,?,?,?,?,?,?)");
+	$stmt->bind_param("issssssss", $ID, $FirstName, $LastName, $StreetAddress, $City, $State, $ZipCode, $PhoneNumber, $Email);
+	$stmt->execute();
+	$stmt->close();
+	$conn->close();
+	returnWithError("");
+}
 
-	function getRequestInfo()
-	{
-		return json_decode(file_get_contents('php://input'), true);
-	}
+function getRequestInfo()
+{
+	return json_decode(file_get_contents('php://input'), true);
+}
 
-	function sendResultInfoAsJson( $obj )
-	{
-		header('Content-type: application/json');
-		echo $obj;
-	}
+function sendResultInfoAsJson( $obj )
+{
+	header('Content-type: application/json');
+	echo $obj;
+}
 
-	function returnWithError( $err )
-	{
-		$retValue = '{"error":"' . $err . '"}';
-		sendResultInfoAsJson( $retValue );
-	}
+function returnWithError( $err )
+{
+	$retValue = '{"error":"' . $err . '"}';
+	sendResultInfoAsJson( $retValue );
+}
 
 ?>
