88525d9d137414bf7a19cb0d1cab5d74a89d3485
Zenarux <84405077+Zenarux@users.noreply.github.com>
2021-05-24 16:47:21 -0400

    Update RemoveContact.php

--- api/RemoveContact.php
+++ api/RemoveContact.php
@@ -1,17 +1,12 @@
 <?php
 
+require 'DBH.php';
 require 'functions.php';
 
 $inData = getRequestInfo();
 
-$serverName = "192.3.62.202";
-$dBUsername = "API";
-$dBPassword = "I4m4robot!";
-$dBName = "cop4331_database";
-
 $ID = $inData["ID"];
 $ContactID = $inData["ContactID"];
-
 
 $conn = new mysqli($serverName, $dBUsername, $dBPassword, $dBName);
 if ($conn->connect_error)
@@ -21,7 +16,7 @@ if ($conn->connect_error)
 else
 {
     # Swap out values for user-inputted values.
-    $sql = ("DELETE FROM Contacts WHERE ID =4 AND ContactID =4");
+    $sql = ("DELETE FROM Contacts WHERE ID =3 AND ContactID =1");
     if ($conn->query($sql) === TRUE) {
         returnWithError("");
     } else {
