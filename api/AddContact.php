a1183bf5320703a59cfc6fcadb3b479fa6ced7f0
Anthonyjackson <56099042+Anthonyjackson@users.noreply.github.com>
2021-05-24 18:42:44 -0400

    Update AddContact.php

--- api/AddContact.php
+++ api/AddContact.php
@@ -1,4 +1,5 @@
   
+  
 <?php
 require 'DBH.php';
 require 'functions.php';
@@ -23,13 +24,10 @@ if ($conn->connect_error)
 else
 {
   # payload should look like this: { "ID": 0, "FirstName": "First", "LastName": "Last", "StreetAddress": "address", "City": "someCity }
+
   
- 
-  
-  
-  
-  $sql = "insert into Contacts (ID, FirstName, LastName, StreetAddress, City,State, ZipCode, PhoneNumber, Email) VALUES ('" . $ID . "', '" . $FirstName . "', '" . $LastName . "', '" . $StreetAddress . "', '" . $City . 
-    "', '" . $State . "', '" . $ZipCode . "', '" . $PhoneNumber . "',  '" . $Email . ") ";
+    $sql = "insert into Contacts (ID,FirstName,LastName,StreetAddress,City,State, ZipCode,PhoneNumber,Email) VALUES (" . $ID . " , '" . $FirstName . "', '" . $LastName . "', '" . $StreetAddress . "', '" . $City . 
+    "', '" . $State . "', '" . $ZipCode . "', '" . $PhoneNumber . "',  '" . $Email . "') ";
 	# insert into Contacts (FirstName,LastName,StreetAddress,City,State,ZipCode,PhoneNumber,Email) VALUES ('Jessica', 'Jones','112 house avenue','',3);
   #what was taught in class
 	#$stmt = $conn->prepare("INSERT into Contacts (ID,FirstName,LastName,StreetAddress,City,State,ZipCode,PhoneNumber,Email) VALUES(?,?,?,?,?,?,?,?,?)");
