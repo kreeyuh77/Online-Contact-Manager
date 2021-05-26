5bf4aea98735f238d3b25b5a05e3ef18bf395dc8
Zenarux <84405077+Zenarux@users.noreply.github.com>
2021-05-26 19:23:23 -0400

    Update EditContact.php

--- api/EditContact.php
+++ api/EditContact.php
@@ -5,7 +5,7 @@ require 'functions.php';
 
 $inData = getRequestInfo();
 
-$edit = $inData["edit"];
+$Edit = $inData["edit"];
 $ID = $inData["ID"];
 $ContactID = $inData["ContactID"];
 $FirstName = "";
@@ -24,35 +24,35 @@ if ($conn->connect_error)
 }
 else
 {
-    if ($edit == "FirstName") {
+    if ($Edit == "FirstName") {
         $stmt = $conn->prepare("UPDATE Contacts SET FirstName=? WHERE ID=?, ContactID=?");
         $stmt->bind_param("iis", $ID, $ContactID, $inData["FirstName"]);
     }
-    if ($edit == "LastName") {
+    if ($Edit == "LastName") {
         $stmt = $conn->prepare("UPDATE Contacts SET LastName=? WHERE ID=?, ContactID=?");
         $stmt->bind_param("iis", $ID, $ContactID, $inData["LastName"]);
     }
-    if ($edit == "StreetAddress") {
+    if ($Edit == "StreetAddress") {
         $stmt = $conn->prepare("UPDATE Contacts SET StreetAddress=? WHERE ID=?, ContactID=?");
         $stmt->bind_param("iis", $ID, $ContactID, $inData["StreetAddress"]);
     }
-    if ($edit == "City") {
+    if ($Edit == "City") {
         $stmt = $conn->prepare("UPDATE Contacts SET City=? WHERE ID=?, ContactID=?");
         $stmt->bind_param("iis", $ID, $ContactID, $inData["City"]);
     }
-    if ($edit == "State") {
+    if ($Edit == "State") {
         $stmt = $conn->prepare("UPDATE Contacts SET State=? WHERE ID=?, ContactID=?");
         $stmt->bind_param("iis", $ID, $ContactID, $inData["State"]);
     }
-    if ($edit == "ZipCode") {
+    if ($Edit == "ZipCode") {
         $stmt = $conn->prepare("UPDATE Contacts SET ZipCode=? WHERE ID=?, ContactID=?");
         $stmt->bind_param("iis", $ID, $ContactID, $inData["ZipCode"]);
     }
-    if ($edit == "PhoneNumber") {
+    if ($Edit == "PhoneNumber") {
         $stmt = $conn->prepare("UPDATE Contacts SET PhoneNumber=? WHERE ID=?, ContactID=?");
         $stmt->bind_param("iis", $ID, $ContactID, $inData["PhoneNumber"]);
     }
-    if ($edit == "Email") {
+    if ($Edit == "Email") {
         $stmt = $conn->prepare("UPDATE Contacts SET Email=? WHERE ID=?, ContactID=?");
         $stmt->bind_param("iis", $ID, $ContactID, $inData["Email"]);
     }
