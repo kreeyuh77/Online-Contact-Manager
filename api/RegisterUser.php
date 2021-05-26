b932b261596ad6a4eecdeeae9e6ebbfd1fe3a6a7
nikhilmsreedhar <65008600+nikhilmsreedhar@users.noreply.github.com>
2021-05-26 18:28:43 -0400

    Update RegisterUser.php

--- api/RegisterUser.php
+++ api/RegisterUser.php
@@ -18,7 +18,7 @@
 	else
 	{
 
-		$stmt = $conn->prepare("INSERT INTO Users (DateCreated, FirstName, LastName, Login, Password) VALUES (?, ?, ?, ?, ?)");
+		$stmt = $conn->prepare("INSERT INTO Users (DateCreated, FirstName, LastName, Login, Password) VALUES (date("Y/m/d"), ?, ?, ?, ?)");
 
 		$stmt->bind_param("ssss", $FirstName, $LastName, $Login, $Password);
 		$stmt->execute();
