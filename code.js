dc4c4f77d0f362331f9c4401b2ab8b9662cbd95c
kreeyuh77 <57364671+kreeyuh77@users.noreply.github.com>
2021-05-26 17:49:29 -0400

    Add files via upload

--- code.js
+++ code.js
@@ -5,7 +5,7 @@ var userId = 0;
 var firstName = "";
 var lastName = "";
 var address = "";
-var city = ""; 
+var city = "";
 var state = "";
 var zipCode = 0;
 var phoneNumber = 0;
@@ -16,11 +16,11 @@ function doLogin()
 	userId = 0;
 	firstName = "";
 	lastName = "";
-	
+
 	var login = document.getElementById("loginName").value;
 	var password = document.getElementById("loginPassword").value;
 //	var hash = md5( password );
-	
+
 	document.getElementById("loginResult").innerHTML = "";
 
 //	var jsonPayload = '{"login" : "' + login + '", "password" : "' + hash + '"}';
@@ -32,26 +32,26 @@ function doLogin()
 	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
 	try
 	{
-		xhr.onreadystatechange = function() 
+		xhr.onreadystatechange = function()
 		{
-			if (this.readyState == 4 && this.status == 200) 
+			if (this.readyState == 4 && this.status == 200)
 			{
 				var jsonObject = JSON.parse( xhr.responseText );
 				userId = jsonObject.id;
-		
+
 				if( userId < 1 )
-				{		
+				{
 					document.getElementById("loginResult").innerHTML = "User/Password combination incorrect";
 					return;
 				}
-		
+
 				firstName = jsonObject.firstName;
 				lastName = jsonObject.lastName;
 
 				saveCookie();
-	
-				window.location.href = "color.html";
-				
+
+				window.location.href = "main.html";
+
 			}
 		};
 		xhr.send(jsonPayload);
@@ -70,13 +70,13 @@ function doSignup()
 	userId = 0;
 	firstName = "";
 	lastName = "";
-	
+
 	var login = document.getElementById("signupName").value;
 	var password = document.getElementById("signupPassword").value;
 
 
 //	var hash = md5( password );
-	
+
 	document.getElementById("signupResult").innerHTML = "";
 
 //	var jsonPayload = '{"login" : "' + login + '", "password" : "' + hash + '"}';
@@ -90,26 +90,26 @@ function doSignup()
 	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
 	try
 	{
-		xhr.onreadystatechange = function() 
+		xhr.onreadystatechange = function()
 		{
-			if (this.readyState == 4 && this.status == 200) 
+			if (this.readyState == 4 && this.status == 200)
 			{
 				var jsonObject = JSON.parse( xhr.responseText );
 				userId = jsonObject.id;
-		
+
 				if(checkPswds(false))
-				{		
+				{
 					document.getElementById("signupResult").innerHTML = "Passwords do not match.";
 					return;
 				}
-		
+
 				firstName = jsonObject.firstName;
 				lastName = jsonObject.lastName;
 
 				saveCookie();
-	
+
 				window.location.href = "color.html";
-				
+
 			}
 		};
 		xhr.send(jsonPayload);
@@ -136,19 +136,19 @@ function doSearch()
 	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
 	try
 	{
-		xhr.onreadystatechange = function() 
+		xhr.onreadystatechange = function()
 		{
-			if (this.readyState == 4 && this.status == 200) 
+			if (this.readyState == 4 && this.status == 200)
 			{
 				var jsonObject = JSON.parse( xhr.responseText );
 				userId = jsonObject.id;
-		
+
 				if( userId < 1 )
-				{		
+				{
 					document.getElementById("searchResult").innerHTML = "No user found in the database.";
 					return;
 				}
-		
+
 				firstName = jsonObject.firstName;
 				lastName = jsonObject.lastName;
 				address = jsonObject.address;
@@ -159,9 +159,9 @@ function doSearch()
 				email = jsonObject.email;
 
 				saveCookie();
-	
+
 				window.location.href = "color.html";
-				
+
 			}
 		};
 		xhr.send(jsonPayload);
@@ -186,19 +186,19 @@ function doDelete()
 	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
 	try
 	{
-		xhr.onreadystatechange = function() 
+		xhr.onreadystatechange = function()
 		{
-			if (this.readyState == 4 && this.status == 200) 
+			if (this.readyState == 4 && this.status == 200)
 			{
 				var jsonObject = JSON.parse( xhr.responseText );
 				userId = jsonObject.id;
-		
+
 				if( userId < 1 )
-				{		
+				{
 					document.getElementById("deleteResult").innerHTML = "User was not found. Unable to delete.";
 					return;
 				}
-		
+
 				firstName = jsonObject.firstName;
 				lastName = jsonObject.lastName;
 				address = jsonObject.address;
@@ -209,9 +209,9 @@ function doDelete()
 				email = jsonObject.email;
 
 				saveCookie();
-	
+
 				window.location.href = "color.html";
-				
+
 			}
 		};
 		xhr.send(jsonPayload);
@@ -237,19 +237,19 @@ function doDeleteSearch()
 	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
 	try
 	{
-		xhr.onreadystatechange = function() 
+		xhr.onreadystatechange = function()
 		{
-			if (this.readyState == 4 && this.status == 200) 
+			if (this.readyState == 4 && this.status == 200)
 			{
 				var jsonObject = JSON.parse( xhr.responseText );
 				userId = jsonObject.id;
-		
+
 				if( userId < 1 )
-				{		
+				{
 					document.getElementById("searchResult").innerHTML = "No user found in the database.";
 					return;
 				}
-		
+
 				firstName = jsonObject.firstName;
 				lastName = jsonObject.lastName;
 				address = jsonObject.address;
@@ -260,9 +260,9 @@ function doDeleteSearch()
 				email = jsonObject.email;
 
 				saveCookie();
-	
+
 				window.location.href = "color.html";
-				
+
 			}
 		};
 		xhr.send(jsonPayload);
@@ -294,19 +294,19 @@ function doAdd()
 	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
 	try
 	{
-		xhr.onreadystatechange = function() 
+		xhr.onreadystatechange = function()
 		{
-			if (this.readyState == 4 && this.status == 200) 
+			if (this.readyState == 4 && this.status == 200)
 			{
 				var jsonObject = JSON.parse( xhr.responseText );
 				userId = jsonObject.id;
-		
+
 /* 				if( userId < 1 )
-				{		
+				{
 					document.getElementById("loginResult").innerHTML = "Unable to add user";
 					return;
 				} */
-		
+
 				firstName = jsonObject.firstName;
 				lastName = jsonObject.lastName;
 				address = jsonObject.address;
@@ -317,9 +317,9 @@ function doAdd()
 				email = jsonObject.email;
 
 				saveCookie();
-	
+
 				window.location.href = "color.html";
-				
+
 			}
 		};
 		xhr.send(jsonPayload);
@@ -334,7 +334,7 @@ function saveCookie()
 {
 	var minutes = 20;
 	var date = new Date();
-	date.setTime(date.getTime()+(minutes*60*1000));	
+	date.setTime(date.getTime()+(minutes*60*1000));
 	document.cookie = "firstName=" + firstName + ",lastName=" + lastName + ",userId=" + userId + ";expires=" + date.toGMTString();
 }
 
@@ -345,7 +345,7 @@ function readCookie()
 	userId = -1;
 	var data = document.cookie;
 	var splits = data.split(",");
-	for(var i = 0; i < splits.length; i++) 
+	for(var i = 0; i < splits.length; i++)
 	{
 		var thisOne = splits[i].trim();
 		var tokens = thisOne.split("=");
@@ -362,7 +362,7 @@ function readCookie()
 			userId = parseInt( tokens[1].trim() );
 		}
 	}
-	
+
 	if( userId < 0 )
 	{
 		window.location.href = "index.html";
@@ -386,18 +386,18 @@ function addColor()
 {
 	var newColor = document.getElementById("colorText").value;
 	document.getElementById("colorAddResult").innerHTML = "";
-	
+
 	var jsonPayload = '{"color" : "' + newColor + '", "userId" : ' + userId + '}';
 	var url = urlBase + '/AddColor.' + extension;
-	
+
 	var xhr = new XMLHttpRequest();
 	xhr.open("POST", url, true);
 	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
 	try
 	{
-		xhr.onreadystatechange = function() 
+		xhr.onreadystatechange = function()
 		{
-			if (this.readyState == 4 && this.status == 200) 
+			if (this.readyState == 4 && this.status == 200)
 			{
 				document.getElementById("colorAddResult").innerHTML = "Color has been added";
 			}
@@ -408,31 +408,31 @@ function addColor()
 	{
 		document.getElementById("colorAddResult").innerHTML = err.message;
 	}
-	
+
 }
 
 function searchColor()
 {
 	var srch = document.getElementById("searchText").value;
 	document.getElementById("colorSearchResult").innerHTML = "";
-	
+
 	var colorList = "";
-	
+
 	var jsonPayload = '{"search" : "' + srch + '","userId" : ' + userId + '}';
 	var url = urlBase + '/SearchColors.' + extension;
-	
+
 	var xhr = new XMLHttpRequest();
 	xhr.open("POST", url, true);
 	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
 	try
 	{
-		xhr.onreadystatechange = function() 
+		xhr.onreadystatechange = function()
 		{
-			if (this.readyState == 4 && this.status == 200) 
+			if (this.readyState == 4 && this.status == 200)
 			{
 				document.getElementById("colorSearchResult").innerHTML = "Color(s) has been retrieved";
 				var jsonObject = JSON.parse( xhr.responseText );
-				
+
 				for( var i=0; i<jsonObject.results.length; i++ )
 				{
 					colorList += jsonObject.results[i];
@@ -441,7 +441,7 @@ function searchColor()
 						colorList += "<br />\r\n";
 					}
 				}
-				
+
 				document.getElementsByTagName("p")[0].innerHTML = colorList;
 			}
 		};
@@ -451,5 +451,5 @@ function searchColor()
 	{
 		document.getElementById("colorSearchResult").innerHTML = err.message;
 	}
-	
+
 }
