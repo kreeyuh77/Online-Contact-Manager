115dd2477fe9fda8dd4906d3596e0df2db962a9e
Zenarux <84405077+Zenarux@users.noreply.github.com>
2021-05-26 18:12:54 -0400

    Update RegisterUser.php

--- api/RegisterUser.php
+++ api/RegisterUser.php
@@ -2,6 +2,11 @@
 	require_once 'DBH.php';
 	require_once 'functions.php';
 
+	$FirstName = $inData["FirstName"];
+	$LastName = $inData["LastName"];
+	$Login = $inData["Login"];
+	$Password = $inData["Password"];
+
 	$conn = new mysqli($serverName, $dBUsername, $dBPassword, $dBName);
 	if( $conn->connect_error )
 	{
@@ -10,7 +15,7 @@
 	else
 	{
 		$stmt = $conn->prepare("INSERT INTO Users (FirstName, LastName, Login, Password) VALUES (?, ?, ?, ?)");
-		$stmt->bind_param("ssss", $inData["FirstName"], $inData["LastName"], $inData["Login"], $inData["Password"]);
+		$stmt->bind_param("ssss", $FirstName, $LastName, $Login, $Password);
 		$stmt->execute();
 		$stmt->close();
 		$conn->close();
