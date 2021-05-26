5260e256fa960317dd243ddb357aafc68fc703a3
Zenarux <84405077+Zenarux@users.noreply.github.com>
2021-05-26 19:36:54 -0400

    Update EditContact.php

--- api/EditContact.php
+++ api/EditContact.php
@@ -5,7 +5,7 @@ require 'functions.php';
 
 $inData = getRequestInfo();
 
-$Edit = $inData["Edit"];
+$edit = $inData["edit"];
 $ID = $inData["ID"];
 $ContactID = $inData["ContactID"];
 $FirstName = "";
@@ -24,37 +24,41 @@ if ($conn->connect_error)
 }
 else
 {
-    if (isset($FirstName)) {
-        $stmt = $conn->prepare("UPDATE Contacts SET FirstName=? WHERE ID=? AND ContactID=?");
-        $stmt->bind_param("iis", $ID, $ContactID, $inData["FirstName"]);
-    }
-    if (isset($LastName)) {
-        $stmt = $conn->prepare("UPDATE Contacts SET LastName=? WHERE ID=? AND ContactID=?");
-        $stmt->bind_param("iis", $ID, $ContactID, $inData["LastName"]);
-    }
-    if (isset($StreetAddress)) {
-        $stmt = $conn->prepare("UPDATE Contacts SET StreetAddress=? WHERE ID=? AND ContactID=?");
-        $stmt->bind_param("iis", $ID, $ContactID, $inData["StreetAddress"]);
-    }
-    if (isset($City)) {
-        $stmt = $conn->prepare("UPDATE Contacts SET City=? WHERE ID=? AND ContactID=?");
-        $stmt->bind_param("iis", $ID, $ContactID, $inData["City"]);
-    }
-    if (isset($State)) {
-        $stmt = $conn->prepare("UPDATE Contacts SET State=? WHERE ID=? AND ContactID=?");
-        $stmt->bind_param("iis", $ID, $ContactID, $inData["State"]);
-    }
-    if (isset($ZipCode)) {
-        $stmt = $conn->prepare("UPDATE Contacts SET ZipCode=? WHERE ID=? AND ContactID=?");
-        $stmt->bind_param("iis", $ID, $ContactID, $inData["ZipCode"]);
-    }
-    if (isset($PhoneNumber)) {
-        $stmt = $conn->prepare("UPDATE Contacts SET PhoneNumber=? WHERE ID=? AND ContactID=?");
-        $stmt->bind_param("iis", $ID, $ContactID, $inData["PhoneNumber"]);
-    }
-    if (isset($Email)) {
-        $stmt = $conn->prepare("UPDATE Contacts SET Email=? WHERE ID=? AND ContactID=?");
-        $stmt->bind_param("iis", $ID, $ContactID, $inData["Email"]);
+    switch ($edit) {
+      case "FirstName":
+        $stmt = $conn->prepare("UPDATE Contacts SET FirstName=? WHERE ID=?, ContactID=?");
+        $stmt->bind_param("sii", $inData["FirstName"],$ID,$ContactID);
+        break;
+      case "LastName":
+        $stmt = $conn->prepare("UPDATE Contacts SET LastName=? WHERE ID=?, ContactID=?");
+        $stmt->bind_param("sii", $inData["LastName"],$ID,$ContactID);
+        break;
+      case "StreetAddress":
+        $stmt = $conn->prepare("UPDATE Contacts SET StreetAddress=? WHERE ID=?, ContactID=?");
+        $stmt->bind_param("sii", $inData["StreetAddress"],$ID,$ContactID);
+        break;
+      case "City":
+        $stmt = $conn->prepare("UPDATE Contacts SET City=? WHERE ID=?, ContactID=?");
+        $stmt->bind_param("sii", $inData["City"],$ID,$ContactID);
+        break;
+      case "State":
+        $stmt = $conn->prepare("UPDATE Contacts SET State=? WHERE ID=?, ContactID=?");
+        $stmt->bind_param("sii", $inData["State"],$ID,$ContactID);
+        break;
+      case "ZipCode":
+        $stmt = $conn->prepare("UPDATE Contacts SET ZipCode=? WHERE ID=?, ContactID=?");
+        $stmt->bind_param("sii", $inData["ZipCode"],$ID,$ContactID);
+        break;
+      case "PhoneNumber":
+        $stmt = $conn->prepare("UPDATE Contacts SET PhoneNumber=? WHERE ID=?, ContactID=?");
+        $stmt->bind_param("sii", $inData["PhoneNumber"],$ID,$ContactID);
+        break;
+      case "Email":
+        $stmt = $conn->prepare("UPDATE Contacts SET Email=? WHERE ID=?, ContactID=?");
+        $stmt->bind_param("sii", $inData["Email"],$ID,$ContactID);
+        break;
+      default:
+        break;
     }
 
     $stmt->execute();
