6a3cafd1117b5cc75ea60d67c159ddcf62c95092
Zenarux <84405077+Zenarux@users.noreply.github.com>
2021-05-26 19:38:02 -0400

    Update EditContact.php

--- api/EditContact.php
+++ api/EditContact.php
@@ -26,35 +26,35 @@ else
 {
     switch ($edit) {
       case "FirstName":
-        $stmt = $conn->prepare("UPDATE Contacts SET FirstName=? WHERE ID=?, ContactID=?");
+        $stmt = $conn->prepare("UPDATE Contacts SET FirstName=? WHERE ID=? AND ContactID=?");
         $stmt->bind_param("sii", $inData["FirstName"],$ID,$ContactID);
         break;
       case "LastName":
-        $stmt = $conn->prepare("UPDATE Contacts SET LastName=? WHERE ID=?, ContactID=?");
+        $stmt = $conn->prepare("UPDATE Contacts SET LastName=? WHERE ID=? AND ContactID=?");
         $stmt->bind_param("sii", $inData["LastName"],$ID,$ContactID);
         break;
       case "StreetAddress":
-        $stmt = $conn->prepare("UPDATE Contacts SET StreetAddress=? WHERE ID=?, ContactID=?");
+        $stmt = $conn->prepare("UPDATE Contacts SET StreetAddress=? WHERE ID=? AND ContactID=?");
         $stmt->bind_param("sii", $inData["StreetAddress"],$ID,$ContactID);
         break;
       case "City":
-        $stmt = $conn->prepare("UPDATE Contacts SET City=? WHERE ID=?, ContactID=?");
+        $stmt = $conn->prepare("UPDATE Contacts SET City=? WHERE ID=? AND ContactID=?");
         $stmt->bind_param("sii", $inData["City"],$ID,$ContactID);
         break;
       case "State":
-        $stmt = $conn->prepare("UPDATE Contacts SET State=? WHERE ID=?, ContactID=?");
+        $stmt = $conn->prepare("UPDATE Contacts SET State=? WHERE ID=? AND ContactID=?");
         $stmt->bind_param("sii", $inData["State"],$ID,$ContactID);
         break;
       case "ZipCode":
-        $stmt = $conn->prepare("UPDATE Contacts SET ZipCode=? WHERE ID=?, ContactID=?");
+        $stmt = $conn->prepare("UPDATE Contacts SET ZipCode=? WHERE ID=? AND ContactID=?");
         $stmt->bind_param("sii", $inData["ZipCode"],$ID,$ContactID);
         break;
       case "PhoneNumber":
-        $stmt = $conn->prepare("UPDATE Contacts SET PhoneNumber=? WHERE ID=?, ContactID=?");
+        $stmt = $conn->prepare("UPDATE Contacts SET PhoneNumber=? WHERE ID=? AND ContactID=?");
         $stmt->bind_param("sii", $inData["PhoneNumber"],$ID,$ContactID);
         break;
       case "Email":
-        $stmt = $conn->prepare("UPDATE Contacts SET Email=? WHERE ID=?, ContactID=?");
+        $stmt = $conn->prepare("UPDATE Contacts SET Email=? WHERE ID=? AND ContactID=?");
         $stmt->bind_param("sii", $inData["Email"],$ID,$ContactID);
         break;
       default:
