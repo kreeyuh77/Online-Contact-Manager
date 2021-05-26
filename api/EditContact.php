e833a76876a11565627d3d38dd1f2319cbbaa1a7
nikhilmsreedhar <65008600+nikhilmsreedhar@users.noreply.github.com>
2021-05-26 19:32:17 -0400

    Update EditContact.php

--- api/EditContact.php
+++ api/EditContact.php
@@ -24,35 +24,35 @@ if ($conn->connect_error)
 }
 else
 {
-    if ($Edit == "FirstName") {
+    if (isset($FirstName)) {
         $stmt = $conn->prepare("UPDATE Contacts SET FirstName=? WHERE ID=?, ContactID=?");
         $stmt->bind_param("iis", $ID, $ContactID, $inData["FirstName"]);
     }
-    if ($Edit == "LastName") {
+    if (isset($LastName)) {
         $stmt = $conn->prepare("UPDATE Contacts SET LastName=? WHERE ID=?, ContactID=?");
         $stmt->bind_param("iis", $ID, $ContactID, $inData["LastName"]);
     }
-    if ($Edit == "StreetAddress") {
+    if (isset($StreetAddress)) {
         $stmt = $conn->prepare("UPDATE Contacts SET StreetAddress=? WHERE ID=?, ContactID=?");
         $stmt->bind_param("iis", $ID, $ContactID, $inData["StreetAddress"]);
     }
-    if ($Edit == "City") {
+    if (isset($City)) {
         $stmt = $conn->prepare("UPDATE Contacts SET City=? WHERE ID=?, ContactID=?");
         $stmt->bind_param("iis", $ID, $ContactID, $inData["City"]);
     }
-    if ($Edit == "State") {
+    if (isset($State)) {
         $stmt = $conn->prepare("UPDATE Contacts SET State=? WHERE ID=?, ContactID=?");
         $stmt->bind_param("iis", $ID, $ContactID, $inData["State"]);
     }
-    if ($Edit == "ZipCode") {
+    if (isset($ZipCode)) {
         $stmt = $conn->prepare("UPDATE Contacts SET ZipCode=? WHERE ID=?, ContactID=?");
         $stmt->bind_param("iis", $ID, $ContactID, $inData["ZipCode"]);
     }
-    if ($Edit == "PhoneNumber") {
+    if (isset($PhoneNumber)) {
         $stmt = $conn->prepare("UPDATE Contacts SET PhoneNumber=? WHERE ID=?, ContactID=?");
         $stmt->bind_param("iis", $ID, $ContactID, $inData["PhoneNumber"]);
     }
-    if ($Edit == "Email") {
+    if (isset($Email)) {
         $stmt = $conn->prepare("UPDATE Contacts SET Email=? WHERE ID=?, ContactID=?");
         $stmt->bind_param("iis", $ID, $ContactID, $inData["Email"]);
     }
