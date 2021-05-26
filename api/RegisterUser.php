f3f478b348be7b6d0bd04edfd3e73f5c8ce74f6a
Zenarux <84405077+Zenarux@users.noreply.github.com>
2021-05-26 17:48:31 -0400

    Update RegisterUser.php

--- api/RegisterUser.php
+++ api/RegisterUser.php
@@ -2,7 +2,6 @@
 	require_once 'DBH.php';
 	require_once 'functions.php';
 
-	$Date = date("m/d/Y");
 	$FirstName = $inData["FirstName"];
 	$LastName = $inData["LastName"];
 	$Login = $inData["Login"];
@@ -15,7 +14,7 @@
 	}
 	else
 	{
-		$stmt = $conn->prepare("INSERT INTO Users (DateCreated, FirstName, LastName, Login, Password) VALUES ($Date, ?, ?, ?, ?)");
+		$stmt = $conn->prepare("INSERT INTO Users (FirstName, LastName, Login, Password) VALUES (?, ?, ?, ?)");
 		$stmt->bind_param("ssss", $FirstName, $LastName, $Login, $Password);
 		$stmt->execute();
 		$stmt->close();
