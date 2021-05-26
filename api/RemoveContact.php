c16eb1f58b4b03ad0051aa84c5fe7196176c0a6b
Zenarux <84405077+Zenarux@users.noreply.github.com>
2021-05-26 16:47:08 -0400

    Update RemoveContact.php

--- api/RemoveContact.php
+++ api/RemoveContact.php
@@ -5,8 +5,9 @@ require_once 'functions.php';
 
 $inData = getRequestInfo();
 
-$ID = $inData["ID"];
-$ContactID = $inData["ContactID"];
+# The json varaible is a placeholder for the value of ID stored on a cookie.
+$ID = $inData["ID"]; # Replace with json_decode($json);
+$ContactID = 0;
 
 $conn = new mysqli($serverName, $dBUsername, $dBPassword, $dBName);
 if ($conn->connect_error)
@@ -15,11 +16,18 @@ if ($conn->connect_error)
 }
 else
 {
-    $stmt = $conn->prepare("DELETE FROM Contacts WHERE ID =? AND ContactID =?");
-    $stmt->bind_param("ii", $ID, $ContactID);
-    $stmt->execute();
-	$stmt->close();
-	$conn->close();
+    $stmt1 = $conn->prepare("SELECT ContactID FROM Contacts WHERE ID =? AND FirstName =? AND LastName =?");
+    $stmt1->bind_param("iss", $ID, $inData["FirstName"], $inData["LastName"]);
+    $stmt1->execute();
+    $ContactID = $stmt1->get_result();
+    $stmt1->close();
+    
+    $stmt2 = $conn->prepare("DELETE FROM Contacts WHERE ID =? AND ContactID =?");
+    $stmt2->bind_param("ii", $ID, $ContactID);
+    $stmt2->execute();
+    $stmt2->close();
+    
+    $conn->close();
     returnWithError("");
 }
 
