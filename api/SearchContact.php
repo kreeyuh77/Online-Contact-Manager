aa3b79c7fed70e35ca6819a8806a67d7ccc942ba
nikhilmsreedhar <65008600+nikhilmsreedhar@users.noreply.github.com>
2021-05-27 13:34:21 -0400

    Update SearchContact.php

--- api/SearchContact.php
+++ api/SearchContact.php
@@ -4,16 +4,17 @@
 
 	$inData = getRequestInfo();
 
+
 	$ID = $inData["ID"];
 	$ContactID = $inData["ContactID"];
-	$FirstName = "";
-	$LastName = "";
-	$StreetAddress = "";
-	$City = "";
-	$State = "";
-	$ZipCode = "";
-	$PhoneNumber = "";
-	$Email = "";
+	$FirstName = $inData["FirstName"];
+	$LastName = $inData["LastName"];
+	$StreetAddress = $inData["StreetAddress"];
+	$City = $inData["City"];
+	$State = $inData["State"];
+	$ZipCode = $inData["ZipCode"];
+	$PhoneNumber = $inData["PhoneNumber"];
+	$Email = $inData["Email"];
 
 	$conn = new mysqli($serverName, $dBUsername, $dBPassword, $dBName);
 	if( $conn->connect_error )
@@ -22,15 +23,50 @@
 	}
 	else
 	{
-		$stmt = $conn->prepare("SELECT FirstName,LastName,StreetAddress,City,State,ZipCode,PhoneNumber,Email FROM Contacts WHERE ID=? AND ContactID=?");
+		$query = "SELECT * FROM Contacts WHERE ID = ? AND ContactID = ?";
+		$stmt = $conn->prepare($query);
 		$stmt->bind_param("ii", $ID, $ContactID);
+		if(isset($FirstName)){
+			$query .= " AND FirstName LIKE '%$FirstName%'";
+		}
+
+		if(isset($LastName)){
+			$query .= " AND LastName LIKE '%$LastName%'";
+		}
+
+		if(isset($StreetAddress)){
+			$query .= " AND StreetAddress LIKE '%$StreetAddress%'";
+		}
+
+		if(isset($City)){
+			$query .= " AND City LIKE '%$City%'";
+		}
+
+		if(isset($State)){
+			$query .= " AND State LIKE '%$State%'";
+		}
+
+		if(isset($ZipCode)){
+			$query .= " AND ZipCode LIKE '%$ZipCode%'";
+		}
+
+		if(isset($PhoneNumber)){
+			$query .= " AND PhoneNumber LIKE '%$PhoneNumber%'";
+		}
+
+		if(isset($Email)){
+			$query .= " AND Email LIKE '%$Email%'";
+		}
 		$stmt->execute();
 		$result = $stmt->get_result();
+
+
 
 		if( $row = $result->fetch_assoc()  )
 		{
 			returnWithInfo($row['FirstName'],$row['LastName'],$row['StreetAddress'],$row['City'],$row['State'],$row['ZipCode'],$row['PhoneNumber'],$row['Email'],);
 		}
+
 		else
 		{
 			returnWithError("No Records Found");
@@ -42,7 +78,6 @@
 
 	function returnWithInfo($FirstName,$LastName,$StreetAddress,$City,$State,$ZipCode,$PhoneNumber,$Email)
 	{
-		$retValue = '{"FirstName": '.$FirstName.',"LastName": '.$LastName.',"StreetAddress": '.$StreetAddress.',"City": '.$City.',"State": '.$State.',"ZipCode": '.$ZipCode.',"PhoneNumber": '.$PhoneNumber.',"Email": '.$Email.',"error": ""}';
+		$retValue = '{"FirstName":"'.$FirstName.'","LastName":"'.$LastName.'","StreetAddress":"'.$StreetAddress.'","City":"'.$City.'","State":"'.$State.'","ZipCode":"'.$ZipCode.'","PhoneNumber":"'.$PhoneNumber.'","Email":"'.$Email.'","error":""}';
 		sendResultInfoAsJson( $retValue );
 	}
-?>
