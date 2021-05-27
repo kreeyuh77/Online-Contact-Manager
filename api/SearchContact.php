2046a1e83226672ba29eb80988bf3e0484298ced
Zenarux <84405077+Zenarux@users.noreply.github.com>
2021-05-27 14:43:45 -0400

    Update SearchContact.php

--- api/SearchContact.php
+++ api/SearchContact.php
@@ -6,7 +6,6 @@
 
 	$ID = $inData["ID"];
 	$ContactID = $inData["ContactID"];
-	$Name = "%".$inData["Name"]."%";
 	$FirstName = $inData["FirstName"];
 	$LastName = $inData["LastName"];
 	$StreetAddress = "";
@@ -17,18 +16,18 @@
 	$Email = "";
 
 	$conn = new mysqli($serverName, $dBUsername, $dBPassword, $dBName);
-	if( $conn->connect_error )
+	if($conn->connect_error)
 	{
-		returnWithError( $conn->connect_error );
+		returnWithError($conn->connect_error);
 	}
 	else
 	{
-		$query = "SELECT * FROM Contacts WHERE ID =? AND ContactID =? AND (FirstName like ? OR LastName like ?)";
+		$query = "SELECT * FROM Contacts WHERE ID =? AND ContactID =? AND FirstName =? AND LastName =?)";
 		$stmt = $conn->prepare($query);
-		$stmt->bind_param("iis", $ID, $ContactID, $Name);
+		$stmt->bind_param("iiss", $ID, $ContactID, $FirstName, $LastName);
 		$stmt->execute();
 		$result = $stmt->get_result();
-		if( $row = $result->fetch_assoc()  )
+		if($row = $result->fetch_assoc())
 		{
 			returnWithInfo($row['FirstName'],$row['LastName'],$row['StreetAddress'],$row['City'],$row['State'],$row['ZipCode'],$row['PhoneNumber'],$row['Email'],);
 		}
@@ -43,5 +42,5 @@
 	function returnWithInfo($FirstName,$LastName,$StreetAddress,$City,$State,$ZipCode,$PhoneNumber,$Email)
 	{
 		$retValue = '{"FirstName":"'.$FirstName.'","LastName":"'.$LastName.'","StreetAddress":"'.$StreetAddress.'","City":"'.$City.'","State":"'.$State.'","ZipCode":"'.$ZipCode.'","PhoneNumber":"'.$PhoneNumber.'","Email":"'.$Email.'","error":""}';
-		sendResultInfoAsJson( $retValue );
+		sendResultInfoAsJson($retValue);
 	}
