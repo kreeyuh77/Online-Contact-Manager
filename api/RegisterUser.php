6c309cd39cfaad7241777ff3bad543fa3d60e973
kdcrist <56696111+kdcrist@users.noreply.github.com>
2021-05-31 18:18:02 -0400

    Create RegisterUser.php

---
+++ api/RegisterUser.php
@@ -0,0 +1,29 @@
+<?php
+	require_once 'DBH.php';
+	require_once 'functions.php';
+
+	$inData = getRequestInfo();
+
+	$DateCreated = date("Y/m/d");
+	$FirstName = $inData["FirstName"];
+	$LastName = $inData["LastName"];
+	$Login = $inData["Login"];
+	$Password = $inData["Password"];
+
+	$conn = new mysqli($serverName, $dBUsername, $dBPassword, $dBName);
+	if( $conn->connect_error )
+	{
+		returnWithError( $conn->connect_error );
+	}
+	else
+	{
+
+		$stmt = $conn->prepare("INSERT INTO Users (DateCreated, FirstName, LastName, Login, Password) VALUES (?, ?, ?, ?, ?)");
+		$stmt->bind_param("sssss", $DateCreated, $FirstName, $LastName, $Login, $Password);
+		$stmt->execute();
+		$stmt->close();
+		$conn->close();
+		returnWithError("($FirstName, $LastName, $Login, $Password)");
+	}
+
+?>
