8d16c6c088949924c1ce9fdeb2887eb15a009d39
nikhilmsreedhar <65008600+nikhilmsreedhar@users.noreply.github.com>
2021-05-31 15:38:22 -0400

    Create doDeleteSearch.js

---
+++ javascript/doDeleteSearch.js
@@ -0,0 +1,56 @@
+function doDeleteSearch()
+{
+	document.getElementById("deleteResult").innerHTML = "";
+	document.getElementById("searchResult").innerHTML = "";
+
+	var jsonPayload = '{"firstName" : "' + firstName + '", "lastName" : "' + lastName + '", "address" : "' + address + '", "city" : "' + city + '", "state" : "' + state + '", "zipCode" : "' + zipCode + '", "phoneNumber" : "' + phoneNumber + '", "email" : "' + email + '"}';
+
+	//Need to edit the url based on the php files given to us
+	var url = urlBase + '/SearchContact.' + extension;
+
+	var xhr = new XMLHttpRequest();
+	xhr.open("GET", url, true);
+	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+	try
+	{
+		xhr.onreadystatechange = function()
+		{
+			if (this.readyState == 4 && this.status == 200)
+			{
+				var jsonObject = JSON.parse( xhr.responseText );
+				userId = jsonObject.id;
+
+				if( userId < 1 )
+				{
+					document.getElementById("searchResult").innerHTML = "No user found in the database.";
+					return;
+				}
+
+				firstName = jsonObject.firstName;
+				lastName = jsonObject.lastName;
+				address = jsonObject.address;
+				city = jsonObject.city;
+				state = jsonObject.state;
+				zipCode = jsonObject.zipCode;
+				phoneNumber = jsonObject.phoneNumber;
+				email = jsonObject.email;
+
+				saveCookie();
+
+				window.location.href = "main.html";
+
+			}
+		};
+		xhr.send(jsonPayload);
+		var xhr = new XMLHttpRequest();
+		xhr.open("DELETE", url, true);
+		xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+		url = urlBase + '/RemoveContact.' + extension;
+		xhr.send(jsonPayload);
+	}
+	catch(err)
+	{
+		document.getElementById("loginResult").innerHTML = err.message;
+	}
+
+}
