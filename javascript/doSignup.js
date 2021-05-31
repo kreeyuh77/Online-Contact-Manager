6ef24a5a17d1c962ed127cba18b2449eacdc445a
Zenarux <84405077+Zenarux@users.noreply.github.com>
2021-05-31 18:34:57 -0400

    Update doSignup.js

--- javascript/doSignup.js
+++ javascript/doSignup.js
@@ -14,7 +14,7 @@ function doSignup()
 	//Need to edit the url based on the php files given to us
 	let url = 'api/RegisterUser.php';
 
-	xhr.open("POST", url, true);
+	xhr.open("POST", url, false);
 	xhr.setRequestHeader("Content-Type", "application/json");
 	//	var jsonPayload = JSON.stringify({"FirstName" : firstName, "LastName" : lastName, "Login" : login, "Password" : hash});
 	//	var jsonPayload = JSON.stringify({"FirstName" : firstName, "LastName" : lastName, "Login" : login, "Password" : password});
