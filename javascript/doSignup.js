960f3aa9d7b4f212fa8b4bb008b7c486ed3f834e
nikhilmsreedhar <65008600+nikhilmsreedhar@users.noreply.github.com>
2021-05-31 18:31:39 -0400

    Update doSignup.js

--- javascript/doSignup.js
+++ javascript/doSignup.js
@@ -1,5 +1,5 @@
-//var urlBase = 'https://wownice.club/api';
-//var extension = 'php';
+var urlBase = 'https://wownice.club/api';
+var extension = 'php';
 
 function doSignup()
 {
@@ -10,10 +10,10 @@ function doSignup()
 
 //	var hash = md5( password );
 	let xhr = new XMLHttpRequest();
-	
+
 	//Need to edit the url based on the php files given to us
 	let url = 'api/RegisterUser.php';
-	
+
 	xhr.open("POST", url, true);
 	xhr.setRequestHeader("Content-Type", "application/json");
 	//	var jsonPayload = JSON.stringify({"FirstName" : firstName, "LastName" : lastName, "Login" : login, "Password" : hash});
