c40445e20c5184093b8975c793d61aeb71f2dcf0
kdcrist <56696111+kdcrist@users.noreply.github.com>
2021-05-31 17:31:34 -0400

    Create doSignup.js

--- javascript/doSignup.js
+++ javascript/doSignup.js
@@ -4,10 +4,10 @@ var extension = '.php';
 function doSignup()
 {
 
-	let firstName = document.getElementById("firstName").value;
-	let lastName = document.getElementById("lastName").value;
-	let login = document.getElementById("signupUsername").value;
-	let password = document.getElementById("signupPassword").value;
+	var firstName = document.getElementById("firstName").value;
+	var lastName = document.getElementById("lastName").value;
+	var login = document.getElementById("signupUsername").value;
+	var password = document.getElementById("signupPassword").value;
 
 //	var hash = md5( password );
 
@@ -16,10 +16,20 @@ function doSignup()
 
 	let xhr = new XMLHttpRequest();
 	xhr.open("POST", url, true);
-	xhr.setRequestHeader("Content-type", "application/json");
+	//xhr.setRequestHeader("Content-type", "application/json");
 	//	var jsonPayload = JSON.stringify({"FirstName" : firstName, "LastName" : lastName, "Login" : login, "Password" : hash});
 	//	var jsonPayload = JSON.stringify({"FirstName" : firstName, "LastName" : lastName, "Login" : login, "Password" : password});
 		var jsonPayload = '{"FirstName" : "' + firstName + '", "LastName" : ' + lastName + ', "Login" : "' + login + '", "Password" : "' + password + '"}';
+		
+	try
+	{
 		xhr.send(jsonPayload);
+		
+		var jsonObject = JSON.parse( xhr.responseText );
+	}
+	catch (err)
+	{
+		document.getElementById("signupResult").innerHTML = err.message;
+	}
 
 }
