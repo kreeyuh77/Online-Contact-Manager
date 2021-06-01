c9fdec44acfe2cd0469be2ecc1baa9d26e6704d6
nikhilmsreedhar <65008600+nikhilmsreedhar@users.noreply.github.com>
2021-06-01 13:33:34 -0400

    Update LoginApi.php

--- api/LoginApi.php
+++ api/LoginApi.php
@@ -4,17 +4,22 @@
 
 	$inData = getRequestInfo();
 
+	# Contact Book login information stored as variables
 	$ID = 0;
 	$FirstName = "";
 	$LastName = "";
 
+	# establish connection to MySQL server to access database and handle failed
+	# connection error case
 	$conn = new mysqli($serverName, $dBUsername, $dBPassword, $dBName);
 	if( $conn->connect_error )
 	{
 		returnWithError( $conn->connect_error );
 	}
+
 	else
 	{
+	
 		$stmt = $conn->prepare("SELECT ID,FirstName,LastName FROM Users WHERE Login=? AND Password =?");
 		$stmt->bind_param("ss", $inData["Login"], $inData["Password"]);
 		$stmt->execute();
