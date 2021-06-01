92312a5df2338fd2d12296df4fddce6b3c05f821
nikhilmsreedhar <65008600+nikhilmsreedhar@users.noreply.github.com>
2021-06-01 13:22:01 -0400

    Update DBH.php

--- api/DBH.php
+++ api/DBH.php
@@ -1,10 +1,12 @@
 <?php
 
+# server/databse information
 $serverName = "192.3.62.202";
 $dBUsername = "API";
 $dBPassword = "I4m4robot!";
 $dBName = "cop4331_database";
 
+#establish connection to MySQL server
 $conn = mysqli_connect($serverName, $dBUsername, $dBPassword, $dBName);
 
 if (!$conn) {
