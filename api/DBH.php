eda494421a9513ef1caaa7809a0cae6850138e2c
nikhilmsreedhar <65008600+nikhilmsreedhar@users.noreply.github.com>
2021-06-01 13:22:56 -0400

    Update DBH.php

--- api/DBH.php
+++ api/DBH.php
@@ -9,6 +9,7 @@ $dBName = "cop4331_database";
 #establish connection to MySQL server
 $conn = mysqli_connect($serverName, $dBUsername, $dBPassword, $dBName);
 
+# error message if script is not able to connect with the server
 if (!$conn) {
 	die("Connection failed: " . mysqli_connect_error());
 }
