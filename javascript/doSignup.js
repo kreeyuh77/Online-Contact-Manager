f57dfba29fc5dfc50dce555452f9125d1e8709c8
kreeyuh77 <57364671+kreeyuh77@users.noreply.github.com>
2021-06-02 17:16:45 -0400

    Update doSignup.js

--- javascript/doSignup.js
+++ javascript/doSignup.js
@@ -19,7 +19,6 @@ function doSignup()
 	//	var jsonPayload = JSON.stringify({"FirstName" : firstName, "LastName" : lastName, "Login" : login, "Password" : hash});
 	//	var jsonPayload = JSON.stringify({"FirstName" : firstName, "LastName" : lastName, "Login" : login, "Password" : password});
 	var jsonPayload = '{"FirstName" : "' + firstName + '", "LastName" : "' + lastName + '", "Login" : "' + login + '", "Password" : "' + hash + '"}';
-	window.location.href = "html/signupsuccess.html"
 	xhr.send(jsonPayload);
 	
 }
