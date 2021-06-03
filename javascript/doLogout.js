2e6e748c2cb695cf26181ff5997c7a09032288ba
kreeyuh77 <57364671+kreeyuh77@users.noreply.github.com>
2021-06-03 19:30:23 -0400

    Update doLogout.js

--- javascript/doLogout.js
+++ javascript/doLogout.js
@@ -4,5 +4,5 @@ function doLogout()
 	var firstName = "";
 	var lastName = "";
 	document.cookie = "firstName= ; expires = Thu, 01 Jan 1970 00:00:00 GMT";
-	window.location.href = "www.wownice.club";
+	window.location.href = "../../index.html";
 }
