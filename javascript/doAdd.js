baecfb72007a682fc98a1437f60522006c355a02
Anthonyjackson <56099042+Anthonyjackson@users.noreply.github.com>
2021-06-03 20:08:16 -0400

    Update doAdd.js

--- javascript/doAdd.js
+++ javascript/doAdd.js
@@ -8,12 +8,14 @@ var zipCode = 0;
 var phoneNumber = 0;
 var email = "";
 
+
+
 function doAdd()
 {
 
 	document.getElementById("addResult").innerHTML = "";
 
-	var jsonPayload = '{"FirstName" : "' + firstName + '", "LastName" : "' + lastName + '", "StreetAddress" : "' + address + '", "City" : "' + city + '", "State" : "' + state + '", "ZipCode" : "' + zipCode + '", "PhoneNumber" : "' + phoneNumber + '", "Email" : "' + email + '"}';
+	var jsonPayload = '{"userId" : "' + userId + '","FirstName" : "' + firstName + '", "LastName" : "' + lastName + '", "StreetAddress" : "' + address + '", "City" : "' + city + '", "State" : "' + state + '", "ZipCode" : "' + zipCode + '", "PhoneNumber" : "' + phoneNumber + '", "Email" : "' + email + '"}';
 
 	//Need to edit the url based on the php files given to us
 	var url = 'api/AddContact.php';
