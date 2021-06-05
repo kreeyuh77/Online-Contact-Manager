32ec08cb3116735e0d5d64f69ce3556838858a4c
nikhilmsreedhar <65008600+nikhilmsreedhar@users.noreply.github.com>
2021-06-05 17:20:59 -0400

    Update RemoveContact.php

--- api/RemoveContact.php
+++ api/RemoveContact.php
@@ -1,33 +1,33 @@
 <?php
 
-require_once 'DBH.php';
-require_once 'functions.php';
+    require_once 'DBH.php';
+    require_once 'functions.php';
 
-$inData = getRequestInfo();
+    $inData = getRequestInfo();
 
-# ID variables used to identify contact to be removed
-# $ID = $inData["ID"];
-$ContactID = $inData["ContactID"];
+    # ID variables used to identify contact to be removed
+    # $ID = $inData["ID"];
+    $ContactID = $inData["ContactID"];
 
 
-# establish connection to MySQL server to access database and handle failed
-# connection error case
-$conn = new mysqli($serverName, $dBUsername, $dBPassword, $dBName);
-if ($conn->connect_error)
-{
-    returnWithError( $conn->connect_error );
-}
+    # establish connection to MySQL server to access database and handle failed
+    # connection error case
+    $conn = new mysqli($serverName, $dBUsername, $dBPassword, $dBName);
+    if ($conn->connect_error)
+    {
+      returnWithError( $conn->connect_error );
+    }
 
-# query the database to select the contact based on the specified variables above
-# and delete that contact from the table. Return an error if encountered
-else
-{
-    $stmt = $conn->prepare("DELETE FROM Contacts WHERE ContactID =?");
-    $stmt->bind_param("i", $ContactID);
-    $stmt->execute();
-    $stmt->close();
-    $conn->close();
-    returnWithError("");
-}
+    # query the database to select the contact based on the specified variables above
+    # and delete that contact from the table. Return an error if encountered
+    else
+    {
+      $stmt = $conn->prepare("DELETE FROM Contacts WHERE ContactID =?");
+      $stmt->bind_param("i", $ContactID);
+      $stmt->execute();
+      $stmt->close();
+      $conn->close();
+      returnWithError("");
+    }
 
 ?>
