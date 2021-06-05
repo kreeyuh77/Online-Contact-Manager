c3a9f62363c573fa1ccee3f7ddd309babec20d61
nikhilmsreedhar <65008600+nikhilmsreedhar@users.noreply.github.com>
2021-06-05 15:24:31 -0400

    Update SearchContact.php

--- api/SearchContact.php
+++ api/SearchContact.php
@@ -38,7 +38,7 @@
 			$stmt->bind_param("i", $ID);
 			$stmt->execute();
 			$result = $stmt->get_result();
-			while(($row = $result->fetch_assoc())
+			while($row = $result->fetch_assoc())
 			{
 				returnWithInfo($row["ContactID"],$row['FirstName'],$row['LastName'],$row['StreetAddress'],$row['City'],$row['State'],$row['ZipCode'],$row['PhoneNumber'],$row['Email']);
 			}
