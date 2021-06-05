5cef7215ede786e149e2b3af0a4cd9aa14fa2e7b
Franklyn Gonzalez <31908148+RollingInCode@users.noreply.github.com>
2021-06-05 17:54:02 -0400

    Update doEditContact.js

--- javascript/doEditContact.js
+++ javascript/doEditContact.js
@@ -1,89 +1,51 @@
 function doEditContact()
 {
-	alert("we called do search!");
-  var jsonPayload = '';
-  var isearch = "";
-	document.getElementById("searchResult").innerHTML = "";
+	alert("editing");
+	var jsonPayload = '';
+	var iedit = "";
+	document.getElementById("editResult").innerHTML = "";
 
-	// get search attritbute
-	var searchText = document.getElementById('search');
-
-	// the list will be put here
-	var contactList = "";
-
-	//  payload depending on searchBy
-	switch (document.getElementById('searchType')){
+	// create edit query
+	var editText = document.getElementById('search');
+	
+	 
+//  editType dicates what you want to edit
+	switch (document.getElementById('editType')){
   case "firstName":
-    isearch = "FirstName";
-    jsonPayload =  '{"search" : "' + isearch + '", "ID" : "' + userId  + '", "FirstName" : "' + searchText + '"}';
+    iedit = "FirstName";
+    jsonPayload =  '{"edit" : "' + iedit + '", "ID" : "' + userId  + '", "FirstName" : "' + editText + '"}';
     break;
   case "lastName":
-    isearch = "LastName";
-    jsonPayload =  '{"search" : "' + isearch + '", "ID" : "' + userId  + '", "LastName" : "' + searchText + '"}';
+    iedit = "LastName";
+    jsonPayload =  '{"edit" : "' + iedit + '", "ID" : "' + userId  + '", "LastName" : "' + editText + '"}';
     break;
   case "address":
-    isearch = "StreetAddress";
-    jsonPayload =  '{"search" : "' + isearch + '", "ID" : "' + userId  + '", "StreetAddress" : "' + searchText + '"}';
+    iedit = "StreetAddress";
+    jsonPayload =  '{"edit" : "' + iedit + '", "ID" : "' + userId  + '", "StreetAddress" : "' + editText + '"}';
     break;
   case "city":
-    isearch = "City";		
-    jsonPayload =  '{"search" : "' + isearch + '", "ID" : "' + userId  + '", "City" : "' + searchText + '"}';
+    iedit = "City";		
+    jsonPayload =  '{"edit" : "' + iedit + '", "ID" : "' + userId  + '", "City" : "' + editText + '"}';
     break;
   case "state":
-    isearch = "State";
-    jsonPayload =  '{"search" : "' + isearch + '", "ID" : "' + userId  + '", "State" : "' + searchText + '"}';
+    iedit = "State";
+    jsonPayload =  '{"edit" : "' + iedit + '", "ID" : "' + userId  + '", "State" : "' + editText + '"}';
     break;
   case "zipcode":
-    isearch = "ZipCode";		
-    jsonPayload =  '{"search" : "' + isearch + '", "ID" : "' + userId  + '", "ZipCode" : "' + searchText + '"}';
+    iedit = "ZipCode";		
+    jsonPayload =  '{"edit" : "' + iedit + '", "ID" : "' + userId  + '", "ZipCode" : "' + editText + '"}';
     break;
   case  "phonenumber":
-    isearch = "PhoneNumber";
-    jsonPayload =  '{"search" : "' + isearch + '", "ID" : "' + userId  + '", "PhoneNumber" : "' + searchText + '"}';
+    iedit = "PhoneNumber";
+    jsonPayload =  '{"edit" : "' + iedit + '", "ID" : "' + userId  + '", "PhoneNumber" : "' + editText + '"}';
   case  "email":
-    isearch = "Email";
-    jsonPayload =  '{"search" : "' + isearch + '", "ID" : "' + userId  + '", "Email" : "' + searchText + '"}';
+    iedit = "Email";
+    jsonPayload =  '{"edit" : "' + iedit + '", "ID" : "' + userId  + '", "Email" : "' + editText + '"}';
 }
-
-	//Need to edit the url based on the php files given to us
-	var url = '../api/SearchContact.php'
-
-	var xhr = new XMLHttpRequest();
-	xhr.open("GET", url, true);
-	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
-	
-	xhr.send(jsonPayload);
-	
-	// 5. DECLARE FUNCTIONS BASED ON EVENTS
-	xhr.onload = (jsonPayload) => {
-  	const data = xhr.response;
-   	console.log(data);
-	};
-
-	xhr.onprogress = (event) => {
-    	console.log(`Loaded ${event.loaded} of ${event.total}`);
-	};
-
-	xhr.onerror = () => {
-    	console.log("Request failed!");
-	};
-
-	
-	
-	var jsonObject = JSON.parse( xhr.responseText );
-    document.getElementById('searchList').innerHTML = jsonObject.FirstName;
-  
-  
-  
-  url = '../api/EditContact.php'
-
-  //table functionality
   
 	var xhr = new XMLHttpRequest();
 	xhr.open("POST", url, true);
 	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
 	
 	xhr.send(jsonPayload);
-	var jsonObject = JSON.parse( xhr.responseText );
-    document.getElementById('searchList').innerHTML = jsonObject.FirstName;
 }
