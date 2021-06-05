f649eaf11012f65c00bc56a5b5e3f102cc670c56
Zenarux <84405077+Zenarux@users.noreply.github.com>
2021-06-05 13:36:22 -0400

    Update RegisterUser.php

--- api/RegisterUser.php
+++ api/RegisterUser.php
@@ -24,7 +24,7 @@
 	else
 	{
 
-		$stmt = $conn->prepare("SELECT * FROM Users WHERE Login =?");
+		$stmt = $conn->prepare("SELECT Login FROM Users WHERE Login =?");
 		$stmt->bind_param("s", $Login);
 		$stmt->execute();
 		$result = $stmt->get_result();
