b360515eb9b52aac180cc9c459861211a661d02d
Zenarux <84405077+Zenarux@users.noreply.github.com>
2021-06-07 18:46:43 -0400

    Update doDelete.js

--- javascript/doDelete.js
+++ javascript/doDelete.js
@@ -1,24 +1,11 @@
-var urlBase = 'http://wownice.club/api';
-var extension = 'php';
-
-var userId = 0;
-var firstName = "";
-var lastName = "";
-var address = "";
-var city = ""; 
-var state = "";
-var zipCode = 0;
-var phoneNumber = 0;
-var email = "";
-
-function doDelete()
+function doDelete(ContactID)
 {
 	document.getElementById("deleteResult").innerHTML = "";
 
 	var jsonPayload = '{"firstName" : "' + firstName + '", "lastName" : "' + lastName + '", "address" : "' + address + '", "city" : "' + city + '", "state" : "' + state + '", "zipCode" : "' + zipCode + '", "phoneNumber" : "' + phoneNumber + '", "email" : "' + email + '"}';
 
 	//Need to edit the url based on the php files given to us
-	var url = urlBase + '/RemoveContact.' + extension;
+	var url = '../api/RemoveContact.php';
 
 	var xhr = new XMLHttpRequest();
 	xhr.open("DELETE", url, true);
