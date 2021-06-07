72bce42cd975ba39aefc64e298155775f3c56bac
kreeyuh77 <57364671+kreeyuh77@users.noreply.github.com>
2021-06-07 19:50:53 -0400

    Update doSearch.js

--- javascript/doSearch.js
+++ javascript/doSearch.js
@@ -178,3 +178,29 @@ function deleteContactHelper(contactID)
 {
     document.getElementById('deleteContact').style.display='block';	
 }
+
+function doDelete(contactID)
+{
+	document.getElementById('deleteContact').style.display='block';
+	document.getElementById("deleteResult").innerHTML = "";
+	document.getElementById("deleteContactButton").addEventListener("click", function() {
+		var jsonPayload = '{"ContactID" : "' + contactID + '"}';
+
+	//Need to edit the url based on the php files given to us
+	var url = '../api/RemoveContact.php';
+
+	var xhr = new XMLHttpRequest();
+	xhr.open("DELETE", url, true);
+	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+	try
+	{	
+		xhr.send(jsonPayload);
+	}
+	catch(err)
+	{
+		document.getElementById("deleteResult").innerHTML = err.message;
+	}
+	});
+	
+	
+}
