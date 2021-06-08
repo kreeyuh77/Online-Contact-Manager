a38f99e2fe98b875b48940a9db2079f3cc10eb5a
kreeyuh77 <57364671+kreeyuh77@users.noreply.github.com>
2021-06-08 00:24:09 -0400

    Update doSignup.js

--- javascript/doSignup.js
+++ javascript/doSignup.js
@@ -1,6 +1,5 @@
 function doSignup()
 {
-	document.getElementById('signup').style.display='block';
 	var firstName = document.getElementById("firstName").value;
 	var lastName = document.getElementById("lastName").value;
 	var login = document.getElementById("signupUsername").value;
