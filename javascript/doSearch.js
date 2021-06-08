a200d2da9881d54c062a8370c9d62b3e6c4bbc2f
Anthonyjackson <56099042+Anthonyjackson@users.noreply.github.com>
2021-06-07 21:26:53 -0400

    Update doSearch.js

--- javascript/doSearch.js
+++ javascript/doSearch.js
@@ -224,7 +224,7 @@ function doEdit(contactID){
 		var url = '../api/EditContact.php';
 
 		var xhr = new XMLHttpRequest();
-		xhr.open("UPDATE", url, true);
+		xhr.open("POST", url, true);
 		xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
 
 		var d = document.getElementById("editType");
