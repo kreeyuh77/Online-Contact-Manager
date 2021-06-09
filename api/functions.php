4229af153238b7f1eb91ed554817020f976d8748
nikhilmsreedhar <65008600+nikhilmsreedhar@users.noreply.github.com>
2021-06-08 23:13:28 -0400

    Update functions.php

--- api/functions.php
+++ api/functions.php
@@ -2,21 +2,20 @@
 
 # returns error as JSON element
 function returnWithError( $err ) {
-   		$retValue = '{"error":"' . $err . '"}';
-   		sendResultInfoAsJson( $retValue );
+  $retValue = '{"error":"' . $err . '"}';
+  sendResultInfoAsJson( $retValue );
 }
 
 # prints result as JSON element
 function sendResultInfoAsJson( $obj ) {
-   		header('Content-type: application/json');
-   		echo $obj;
+  header('Content-type: application/json');
+  echo $obj;
 }
 
 # receive JSON data as a PHP file and read the file into a string, then decode
 # the JSON string
 function getRequestInfo()
-	{
-		return json_decode(file_get_contents('php://input'), true);
-	}
-
+{
+  return json_decode(file_get_contents('php://input'), true);
+}
 ?>
