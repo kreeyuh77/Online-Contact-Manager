c370194325ea4bfeed160f44b4c7f5566679e53d
nikhilmsreedhar <65008600+nikhilmsreedhar@users.noreply.github.com>
2021-06-08 23:32:33 -0400

    Update RegisterUser.php

--- api/RegisterUser.php
+++ api/RegisterUser.php
@@ -1,48 +1,48 @@
-<?php
-	require_once 'DBH.php';
-	require_once 'functions.php';
+	<?php
+		require_once 'DBH.php';
+		require_once 'functions.php';
 
-	$inData = getRequestInfo();
+		$inData = getRequestInfo();
 
-	# Contact Book User registration information stored as variables.
-	$DateCreated = date("Y/m/d");
-	$FirstName = $inData["FirstName"];
-	$LastName = $inData["LastName"];
-	$Login = $inData["Login"];
-	$Password = $inData["Password"];
+		# Contact Book User registration information stored as variables.
+		$DateCreated = date("Y/m/d");
+		$FirstName = $inData["FirstName"];
+		$LastName = $inData["LastName"];
+		$Login = $inData["Login"];
+		$Password = $inData["Password"];
 
-	# establish connection to MySQL server to access database and handle failed
-	# connection error case
-	$conn = new mysqli($serverName, $dBUsername, $dBPassword, $dBName);
-	if( $conn->connect_error )
-	{
-		returnWithError( $conn->connect_error );
-	}
-
-	# Query the database to insert the registered user into the Users table if
-	# validation constraints are met or else return an error
-	else
-	{
-		$stmt = $conn->prepare("SELECT * FROM Users WHERE Login =?");
-		$stmt->bind_param("s", $Login);
-		$stmt->execute();
-		$result = $stmt->get_result();
-		$row = $result->fetch_assoc();
-		$stmt->close();
-		
-		if ($Login == $row["Login"])
+		# establish connection to MySQL server to access database and handle failed
+		# connection error case
+		$conn = new mysqli($serverName, $dBUsername, $dBPassword, $dBName);
+		if( $conn->connect_error )
 		{
-			returnWithError("Username already exists.");
+			returnWithError( $conn->connect_error );
 		}
+
+		# Query the database to insert the registered user into the Users table if
+		# validation constraints are met or else return an error
 		else
 		{
-			$stmt = $conn->prepare("INSERT INTO Users (DateCreated, FirstName, LastName, Login, Password) VALUES (?, ?, ?, ?, ?)");
-			$stmt->bind_param("sssss", $DateCreated, $FirstName, $LastName, $Login, $Password);
+			$stmt = $conn->prepare("SELECT * FROM Users WHERE Login =?");
+			$stmt->bind_param("s", $Login);
 			$stmt->execute();
+			$result = $stmt->get_result();
+			$row = $result->fetch_assoc();
 			$stmt->close();
-			$conn->close();
-			returnWithError("");
-		}
-	}
 
-?>
+			if ($Login == $row["Login"])
+			{
+				returnWithError("Username already exists.");
+			}
+			else
+			{
+				$stmt = $conn->prepare("INSERT INTO Users (DateCreated, FirstName, LastName, Login, Password) VALUES (?, ?, ?, ?, ?)");
+				$stmt->bind_param("sssss", $DateCreated, $FirstName, $LastName, $Login, $Password);
+				$stmt->execute();
+				$stmt->close();
+				$conn->close();
+				returnWithError("");
+			}
+		}
+
+	?>
