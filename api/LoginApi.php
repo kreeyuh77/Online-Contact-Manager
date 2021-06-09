9a73457f9e5529abf1acaf1e18de787edd5cbb29
nikhilmsreedhar <65008600+nikhilmsreedhar@users.noreply.github.com>
2021-06-08 23:32:24 -0400

    Update LoginApi.php

--- api/LoginApi.php
+++ api/LoginApi.php
@@ -1,50 +1,50 @@
-<?php
-	require_once 'DBH.php';
-	require_once 'functions.php';
+	<?php
+		require_once 'DBH.php';
+		require_once 'functions.php';
 
-	$inData = getRequestInfo();
+		$inData = getRequestInfo();
 
-	# Contact Book login information stored as variables
-	$ID = 0;
-	$FirstName = "";
-	$LastName = "";
+		# Contact Book login information stored as variables
+		$ID = 0;
+		$FirstName = "";
+		$LastName = "";
 
-	# establish connection to MySQL server to access database and handle failed
-	# connection error case
-	$conn = new mysqli($serverName, $dBUsername, $dBPassword, $dBName);
-	if( $conn->connect_error )
-	{
-		returnWithError( $conn->connect_error );
-	}
-
-	# Load the SQL query into a variable and append the corresponding parameters
-	# and return information if Login is valid based on returned row or else return an error
-	else
-	{
-		$stmt = $conn->prepare("SELECT ID,FirstName,LastName FROM Users WHERE Login=? AND Password =?");
-		$stmt->bind_param("ss", $inData["Login"], $inData["Password"]);
-		$stmt->execute();
-		$result = $stmt->get_result();
-
-		if( $row = $result->fetch_assoc()  )
+		# establish connection to MySQL server to access database and handle failed
+		# connection error case
+		$conn = new mysqli($serverName, $dBUsername, $dBPassword, $dBName);
+		if( $conn->connect_error )
 		{
-			returnWithInfo( $row['FirstName'], $row['LastName'], $row['ID'] );
+			returnWithError( $conn->connect_error );
 		}
+
+		# Load the SQL query into a variable and append the corresponding parameters
+		# and return information if Login is valid based on returned row or else return an error
 		else
 		{
-			returnWithError("No Records Found");
+			$stmt = $conn->prepare("SELECT ID,FirstName,LastName FROM Users WHERE Login=? AND Password =?");
+			$stmt->bind_param("ss", $inData["Login"], $inData["Password"]);
+			$stmt->execute();
+			$result = $stmt->get_result();
+
+			if( $row = $result->fetch_assoc()  )
+			{
+				returnWithInfo( $row['FirstName'], $row['LastName'], $row['ID'] );
+			}
+			else
+			{
+				returnWithError("No Records Found");
+			}
+
+			$stmt->close();
+			$conn->close();
 		}
 
-		$stmt->close();
-		$conn->close();
-	}
+		# obtain the login information based on the input parameters and send information
+		# as JSON element.
+		function returnWithInfo( $FirstName, $LastName, $ID )
+		{
+			$retValue = '{"ID":' . $ID . ',"FirstName":"' . $FirstName . '","LastName":"' . $LastName . '","error":""}';
+			sendResultInfoAsJson( $retValue );
+		}
 
-	# obtain the login information based on the input parameters and send information
-	# as JSON element.
-	function returnWithInfo( $FirstName, $LastName, $ID )
-	{
-		$retValue = '{"ID":' . $ID . ',"FirstName":"' . $FirstName . '","LastName":"' . $LastName . '","error":""}';
-		sendResultInfoAsJson( $retValue );
-	}
-
-?>
+	?>
