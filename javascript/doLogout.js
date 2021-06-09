92aeae11d91d4e4ada8d4971bd43f761c9ad69e4
nikhilmsreedhar <65008600+nikhilmsreedhar@users.noreply.github.com>
2021-06-08 23:07:01 -0400

    Update doLogout.js

--- javascript/doLogout.js
+++ javascript/doLogout.js
@@ -1,8 +1,8 @@
-function doLogout()
-{
-	var userId = 0;
-	var firstName = "";
-	var lastName = "";
-	document.cookie = "firstName= ; expires = Thu, 01 Jan 1970 00:00:00 GMT";
-	window.location.href = "../index.html";
-}
+		function doLogout()
+		{
+			var userId = 0;
+			var firstName = "";
+			var lastName = "";
+			document.cookie = "firstName= ; expires = Thu, 01 Jan 1970 00:00:00 GMT";
+			window.location.href = "../index.html";
+		}
