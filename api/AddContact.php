0310668dfdca55d1e6150ed327a0c56b9099b979
nikhilmsreedhar <65008600+nikhilmsreedhar@users.noreply.github.com>
2021-06-08 23:31:58 -0400

    Update AddContact.php

--- api/AddContact.php
+++ api/AddContact.php
@@ -1,34 +1,34 @@
-<?php
-require_once 'DBH.php';
-require_once 'functions.php';
+	<?php
+	require_once 'DBH.php';
+	require_once 'functions.php';
 
-$inData = getRequestInfo();
+	$inData = getRequestInfo();
 
-# ID needs to obtain value from JSON.
-$ID = $inData["ID"];
-$FirstName = $inData["FirstName"];
-$LastName = $inData["LastName"];
-$StreetAddress = $inData["StreetAddress"];
-$City = $inData["City"];
-$State = $inData["State"];
-$ZipCode = $inData["ZipCode"];
-$PhoneNumber= $inData["PhoneNumber"];
-$Email = $inData["Email"];
+	# ID needs to obtain value from JSON.
+	$ID = $inData["ID"];
+	$FirstName = $inData["FirstName"];
+	$LastName = $inData["LastName"];
+	$StreetAddress = $inData["StreetAddress"];
+	$City = $inData["City"];
+	$State = $inData["State"];
+	$ZipCode = $inData["ZipCode"];
+	$PhoneNumber= $inData["PhoneNumber"];
+	$Email = $inData["Email"];
 
-# establish connection to MySQL server to access database and handle failed
-# connection error case
-$conn = new mysqli($serverName, $dBUsername, $dBPassword, $dBName);
-if ($conn->connect_error)
-{
-	returnWithError( $conn->connect_error );
-}
-else
-{
-	# insert into Contacts (FirstName,LastName,StreetAddress,City,State,ZipCode,PhoneNumber,Email) VALUES ('Jessica', 'Jones','112 house avenue','',3);
-	$stmt = $conn->prepare("INSERT into Contacts (ID,FirstName,LastName,StreetAddress,City,State,ZipCode,PhoneNumber,Email) VALUES(?,?,?,?,?,?,?,?,?)");
-	$stmt->bind_param("sssssssss", $ID, $FirstName, $LastName, $StreetAddress, $City, $State, $ZipCode, $PhoneNumber, $Email);
-	$stmt->execute();
-	$stmt->close();
-	$conn->close();
-	returnWithError("");
-}
+	# establish connection to MySQL server to access database and handle failed
+	# connection error case
+	$conn = new mysqli($serverName, $dBUsername, $dBPassword, $dBName);
+	if ($conn->connect_error)
+	{
+		returnWithError( $conn->connect_error );
+	}
+	else
+	{
+		# insert into Contacts (FirstName,LastName,StreetAddress,City,State,ZipCode,PhoneNumber,Email) VALUES ('Jessica', 'Jones','112 house avenue','',3);
+		$stmt = $conn->prepare("INSERT into Contacts (ID,FirstName,LastName,StreetAddress,City,State,ZipCode,PhoneNumber,Email) VALUES(?,?,?,?,?,?,?,?,?)");
+		$stmt->bind_param("sssssssss", $ID, $FirstName, $LastName, $StreetAddress, $City, $State, $ZipCode, $PhoneNumber, $Email);
+		$stmt->execute();
+		$stmt->close();
+		$conn->close();
+		returnWithError("");
+	}
