2be9c51f46c01b02d08f3efe66f9f84937dcdfbc
nikhilmsreedhar <65008600+nikhilmsreedhar@users.noreply.github.com>
2021-06-08 23:32:02 -0400

    Update DBH.php

--- api/DBH.php
+++ api/DBH.php
@@ -1,16 +1,16 @@
-<?php
+	<?php
 
-# server/database information
-$serverName = "192.3.62.202";
-$dBUsername = "API";
-$dBPassword = "I4m4robot!";
-$dBName = "cop4331_database";
+	# server/database information
+	$serverName = "192.3.62.202";
+	$dBUsername = "API";
+	$dBPassword = "I4m4robot!";
+	$dBName = "cop4331_database";
 
-#establish connection to MySQL server
-$conn = mysqli_connect($serverName, $dBUsername, $dBPassword, $dBName);
+	#establish connection to MySQL server
+	$conn = mysqli_connect($serverName, $dBUsername, $dBPassword, $dBName);
 
-# error message if script is not able to connect with the server
-if (!$conn) {
-	die("Connection failed: " . mysqli_connect_error());
-}
-?>
+	# error message if script is not able to connect with the server
+	if (!$conn) {
+		die("Connection failed: " . mysqli_connect_error());
+	}
+	?>
