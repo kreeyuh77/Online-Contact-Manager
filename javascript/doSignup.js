96cf13f05125e60c3321f559ef8ee3d02fc1e53f
nikhilmsreedhar <65008600+nikhilmsreedhar@users.noreply.github.com>
2021-06-08 23:10:23 -0400

    Update doSignup.js

--- javascript/doSignup.js
+++ javascript/doSignup.js
@@ -1,15 +1,11 @@
 function doSignup()
 {
-
-	
 	var firstName = document.getElementById("firstName").value;
 	var lastName = document.getElementById("lastName").value;
 	var login = document.getElementById("signupUsername").value;
 	var password = document.getElementById("signupPassword").value;
-
 	var hash = md5( password );
 	let xhr = new XMLHttpRequest();
-
 	//Need to edit the url based on the php files given to us
 	let url = 'api/RegisterUser.php';
 
@@ -18,6 +14,4 @@ function doSignup()
 	//var jsonPayload = JSON.stringify({"FirstName" : firstName, "LastName" : lastName, "Login" : login, "Password" : hash});
 	var jsonPayload = '{"FirstName" : "' + firstName + '", "LastName" : "' + lastName + '", "Login" : "' + login + '", "Password" : "' + hash + '"}';
 	xhr.send(jsonPayload);
-	
-	
 }
