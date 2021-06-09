909a012d9e9e6fecbc257c6c710f92c7c8d7d317
nikhil sreedhar <65008600+nikhilmsreedhar@users.noreply.github.com>
2021-06-08 22:35:52 -0400

    Update doAdd.js

--- javascript/doAdd.js
+++ javascript/doAdd.js
@@ -1,22 +1,17 @@
 function doAdd()
 {
-var firstName = document.getElementById("firstName").value;
-var lastName = document.getElementById("lastName").value;
-var address = document.getElementById("address").value;
-var city = document.getElementById("city").value;
-var state = document.getElementById("state").value;
-var zipcode = document.getElementById("zipcode").value;
-var phonenumber = document.getElementById("phonenumber").value;
-var email = document.getElementById("email").value;
-	
-	
+	var firstName = document.getElementById("firstName").value;
+	var lastName = document.getElementById("lastName").value;
+	var address = document.getElementById("address").value;
+	var city = document.getElementById("city").value;
+	var state = document.getElementById("state").value;
+	var zipcode = document.getElementById("zipcode").value;
+	var phonenumber = document.getElementById("phonenumber").value;
+	var email = document.getElementById("email").value;
 	document.getElementById("addResult").innerHTML = "";
-
+	
 	var jsonPayload = '{"ID" : "' + userId + '","FirstName" : "' + firstName + '", "LastName" : "' + lastName + '", "StreetAddress" : "' + address + '", "City" : "' + city + '", "State" : "' + state + '", "ZipCode" : "' + zipcode + '", "PhoneNumber" : "' + phonenumber + '", "Email" : "' + email + '"}';
-
-	//Need to edit the url based on the php files given to us
 	var url = '../api/AddContact.php';
-
 	var xhr = new XMLHttpRequest();
 	xhr.open("POST", url, true);
 	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
